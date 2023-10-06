efd556fef337ff23d4f1b42deddfa1aa7a76dfa0
Bhavin <bhavinpatanwadia99@gmail.com>
2023-10-06 16:29:39 -0400

    Database connected

--- server/app.js
+++ server/app.js
@@ -2,7 +2,10 @@ import express from "express";
 import configRoutes from "./routes/index.js";
 import "dotenv/config.js";
 import { corsConfig } from "./config/settings.js";
+import helmet from "helmet";
 import cors from "cors";
+import { connectDB } from "./config/dbConnection.js";
+
 
 const app = express();
 
@@ -10,11 +13,18 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 app.use(cors(corsConfig));
-
+app.use(helmet());
 configRoutes(app);
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log("Server is listening on port 4000 localhost");
-})
-
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URI);
+      console.log("Database is connected");
+      app.listen(process.env.PORT, ()=>{
+        console.log("Server is listening on port 4000 localhost");
+        });    
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  start(); 
