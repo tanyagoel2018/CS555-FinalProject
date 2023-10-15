3063056564e57542cc74bc898dde35c365f0f467
Bhavin <bhavinpatanwadia99@gmail.com>
2023-10-14 22:23:17 -0400

    dbcollection and data folder added

---
+++ server/config/dbCollections.js
@@ -0,0 +1,16 @@
+import { getClient } from "./dbConnection.js";
+const getCollectionFn = (collection) => {
+    let _col = undefined;
+  
+    return async () => {
+      if (!_col) {
+         const db = await getClient()
+        _col = await db.collection(collection);
+      }
+  
+      return _col;
+    };
+  };
+  
+  export const users = getCollectionFn('Users');
+  
