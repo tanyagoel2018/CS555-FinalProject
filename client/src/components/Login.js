b9e217dd107199fe0a49a899b29a163a42f5ef96
tanyagoel2018 <tanyagoel2018@gmail.com>
2023-10-17 20:36:23 -0400

    login front-end done

---
+++ client/src/components/Login.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { restAPI } from "../service/api";
+import { useFormik } from "formik";
+import { loginSchema } from "../validations/userValidation";
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  TextField,
+  CssBaseline,
+  Box,
+  Grid,
+  Typography,
+  Backdrop,
+  CircularProgress,
+  Snackbar,
+  Alert,
+  Link,
+} from "@mui/material";
+
+const Login = () => {
+  const [loader, setLoader] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setError(false);
+    setSuccess(false);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      setLoader(true);
+      restAPI
+        .post("/login", values)
+        .then((response) => {
+          setLoader(false);
+          setSuccess(true);
+        })
+        .catch((error) => {
+          setLoader(false);
+          setError(true);
+        });
+    },
+  });
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Snackbar open={success} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          You are all set!
+        </Alert>
+      </Snackbar>
+      <Snackbar open={error} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          Something went wrong!
+        </Alert>
+      </Snackbar>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar> */}
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loader}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+        <Box sx={{ mt: 3 }}>
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  id="email"
+                  name="email"
+                  label="Email"
+                  fullWidth
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  id="password"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+          </form>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Don't have an account? Sign up here!
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
--- server/app.js
+++ server/app.js
@@ -6,10 +6,9 @@ import helmet from "helmet";
 import cors from "cors";
 import { connectDB } from "./config/dbConnection.js";
 
-
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(cors(corsConfig));
@@ -17,14 +16,14 @@ app.use(helmet());
 configRoutes(app);
 
 const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URI);
-      console.log("Database is connected");
-      app.listen(process.env.PORT, ()=>{
-        console.log("Server is listening on port 4000 localhost");
-        });    
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  start(); 
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("Database is connected");
+    app.listen(process.env.PORT, () => {
+      console.log("Server is listening on port 4000 localhost");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
