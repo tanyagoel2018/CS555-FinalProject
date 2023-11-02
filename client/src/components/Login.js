0ca82b2e5bb0d33d76149a2816ff349c502e1dd6
tanyagoel2018 <tanyagoel2018@gmail.com>
2023-11-01 20:22:19 -0400

    refactored login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import { loginSchema } from "../validations/userValidation";
 import { useNavigate, Link } from "react-router-dom";
+import CustomSnackbar from "./CustomSnackbar";
 import {
   Button,
   Container,
@@ -12,16 +13,13 @@ import {
   Typography,
   Backdrop,
   CircularProgress,
-  Snackbar,
-  Alert,
 } from "@mui/material";
 import { useApi } from "../ContextAPI/APIContext";
+import useSnackbar from "../hooks/useSnackbar";
 
 const Login = () => {
   const [loader, setLoader] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("Something went wrong");
+  const snackbar = useSnackbar();
 
   const navigate = useNavigate();
   const { restAPI } = useApi();
@@ -33,56 +31,35 @@ const Login = () => {
     }
   }, [navigate]);
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setError(false);
-    setSuccess(false);
-  };
-
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
     validationSchema: loginSchema,
-    onSubmit: (values) => {
-      console.log(values);
-      setLoader(true);
-
-      restAPI
-        .post("/login", values)
-        .then((response) => {
-          setLoader(false);
-          console.log(response);
-          setSuccess(true);
-
-          localStorage.setItem("Are_you_in", "yes");
-          navigate("/home");
-        })
-        .catch((error) => {
-          setLoader(false);
-          setError(true);
-          if (error && error.response && error.response.data) {
-            setErrorMsg(error.response.data);
-          }
-        });
-    },
+    onSubmit: handleSubmit,
   });
+
+  function handleSubmit(values) {
+    setLoader(true);
+
+    restAPI
+      .post("/login", values)
+      .then((response) => {
+        snackbar.showSuccess("Login successful!");
+
+        localStorage.setItem("Are_you_in", "yes");
+        navigate("/home");
+      })
+      .catch((error) => {
+        if (error && error.response && error.response.data) {
+          snackbar.showError(error.response.data);
+        }
+      });
+  }
 
   return (
     <Container component="main" maxWidth="xs">
-      <Snackbar open={success} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-          Login successful!
-        </Alert>
-      </Snackbar>
-      <Snackbar open={error} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-          {errorMsg}
-        </Alert>
-      </Snackbar>
       <CssBaseline />
       <Box
         sx={{
@@ -92,9 +69,6 @@ const Login = () => {
           alignItems: "center",
         }}
       >
-        {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar> */}
         <Typography component="h1" variant="h5">
           Login
         </Typography>
@@ -107,37 +81,8 @@ const Login = () => {
         <Box sx={{ mt: 3 }}>
           <form onSubmit={formik.handleSubmit}>
             <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  id="email"
-                  name="email"
-                  label="Email"
-                  fullWidth
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  id="password"
-                  name="password"
-                  label="Password"
-                  type="password"
-                  fullWidth
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={
-                    formik.touched.password && Boolean(formik.errors.password)
-                  }
-                  helperText={formik.touched.password && formik.errors.password}
-                />
-              </Grid>
+              {renderTextField("email", "Email", "email")}
+              {renderTextField("password", "Password", "password")}
             </Grid>
             <Button
               type="submit"
@@ -155,10 +100,31 @@ const Login = () => {
               </Link>
             </Grid>
           </Grid>
+          <CustomSnackbar snackbarProp={snackbar} />
         </Box>
       </Box>
     </Container>
   );
+
+  function renderTextField(id, label, type) {
+    return (
+      <Grid item xs={12}>
+        <TextField
+          variant="outlined"
+          id={id}
+          name={id}
+          label={label}
+          type={type}
+          fullWidth
+          value={formik.values[id]}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched[id] && Boolean(formik.errors[id])}
+          helperText={formik.touched[id] && formik.errors[id]}
+        />
+      </Grid>
+    );
+  }
 };
 
 export default Login;
--- server/test/signup.js
+++ server/test/signup.js
@@ -1,33 +1,32 @@
-import request from 'supertest';
-import app from '../app.js';
-import { expect } from 'chai';
+import request from "supertest";
+import app from "../app.js";
+import { expect } from "chai";
 
-
-describe('Sign-up Form API Test', function () {
-  it('should return 200 status when all fields are valid', function (done) {
+describe("Sign-up Form API Test", function () {
+  it("should return 200 status when all fields are valid", function (done) {
     request(app)
-      .post('/sign-up')
+      .post("/sign-up")
       .send({
-        name: 'John Doe',
-        email: 'JOhnAmerica@gmail.com',
-        password: 'P@ssw0rd123',
+        name: "John Moe",
+        email: "JOhnMoeAmerica@gmail.com",
+        password: "P@ssw0rd123",
         age: 25,
       })
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body).to.equal('Registration successful');
+        expect(res.body).to.equal("Registration successful");
         done();
       });
   });
 
-  it('should return 400 status when the name field is empty', function (done) {
+  it("should return 400 status when the name field is empty", function (done) {
     request(app)
-      .post('/sign-up')
+      .post("/sign-up")
       .send({
-        name: '',
-        email: 'john@example.com',
-        password: 'P@ssw0rd',
+        name: "",
+        email: "john@example.com",
+        password: "P@ssw0rd",
         age: 25,
       })
       .expect(400)
@@ -38,13 +37,13 @@ describe('Sign-up Form API Test', function () {
       });
   });
 
-  it('should return 400 status when the email format is invalid', function (done) {
+  it("should return 400 status when the email format is invalid", function (done) {
     request(app)
-      .post('/sign-up')
+      .post("/sign-up")
       .send({
-        name: 'John Doe',
-        email: 'invalid-email',
-        password: 'P@ssw0rd123',
+        name: "John Doe",
+        email: "invalid-email",
+        password: "P@ssw0rd123",
         age: 25,
       })
       .expect(400)
@@ -55,31 +54,33 @@ describe('Sign-up Form API Test', function () {
       });
   });
 
-  it('should return 400 status when the password is weak', function (done) {
+  it("should return 400 status when the password is weak", function (done) {
     request(app)
-      .post('/sign-up')
+      .post("/sign-up")
       .send({
-        name: 'John Doe',
-        email: 'john@example.com',
-        password: 'weak',
+        name: "John Doe",
+        email: "john@example.com",
+        password: "weak",
         age: 25,
       })
       .expect(400)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body.message).to.equal("password must contain at least 1 uppercase letter");
+        expect(res.body.message).to.equal(
+          "password must contain at least 1 uppercase letter"
+        );
         done();
       });
   });
 
-  it('should return 400 status when the age is not a number', function (done) {
+  it("should return 400 status when the age is not a number", function (done) {
     request(app)
-      .post('/sign-up')
+      .post("/sign-up")
       .send({
-        name: 'John Doe',
-        email: 'john@example.com',
-        password: 'P@ssw0rd123',
-        age: 'r',
+        name: "John Doe",
+        email: "john@example.com",
+        password: "P@ssw0rd123",
+        age: "r",
       })
       .expect(400)
       .end((err, res) => {
@@ -89,12 +90,12 @@ describe('Sign-up Form API Test', function () {
       });
   });
 
-  it('should return 400 status when any field is missing', function (done) {
+  it("should return 400 status when any field is missing", function (done) {
     request(app)
-      .post('/sign-up')
+      .post("/sign-up")
       .send({
-        email: 'john@example.com',
-        password: 'P@ssw0rd123',
+        email: "john@example.com",
+        password: "P@ssw0rd123",
         age: 25,
       })
       .expect(400)
